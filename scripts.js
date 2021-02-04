6445beddc14293db3305c179b399f2d552aceced
Leandro Dias <leandrodias.me@gmail.com>
2021-02-04 18:11:31 -0400

    Correção do bug 56.0000

--- scripts.js
+++ scripts.js
@@ -119,8 +119,8 @@ const Utils = {
         return signal + value
     },
     formatAmount(value) {
-        value = Number(value) * 100
-        return value
+        value = Number(value) * 100;
+        return Math.round(value);
     },
     formatDate(date) {
         const splittedDate = date.split('-')

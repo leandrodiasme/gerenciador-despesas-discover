359378d87d453fd90a9ba2acc6c8ebc8a38e41d8
Leandro Dias <leandrodias.me@gmail.com>
2021-02-07 00:22:02 -0400

    Função para imprimir a tabela

--- scripts.js
+++ scripts.js
@@ -18,19 +18,6 @@ const Storage = {
     }
 }
 
-function printData()
-{
-   var divToPrint=document.getElementById("data-table");
-   newWin= window.open("");
-   newWin.document.write(divToPrint.outerHTML);
-   newWin.print();
-   newWin.close();
-}
-
-// $('button').on('click',function(){
-// printData();
-// })
-
 const Transaction = {
     all: Storage.get(),
     
